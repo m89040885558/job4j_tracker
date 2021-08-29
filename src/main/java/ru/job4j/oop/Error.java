b8d0c6d280fb67f6ed3076a5cee3ca7e6995f786
m89040885558 <m89040885558@yandex.ru>
2021-08-29 12:03:37 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,29 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+     public Error() {
+     }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("Доступ: " + active);
+        System.out.println("Код ощибки: " + status + " GB");
+        System.out.println("Сообщение: " + message);
+        }
+
+    public static void main(String[] args) {
+        Error error = new Error(true, 10406, "Critical ERrOR");
+        Error error_default = new Error();
+        error.printInfo();
+        error_default.printInfo();
+    }
+}
