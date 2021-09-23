433e46e8b9e316d5de4bec171e2babc027bc889c
m89040885558 <m89040885558@yandex.ru>
2021-09-23 19:46:45 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,9 +1,26 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
     private int size = 0;
+
+    public Item[] findAll() {
+        return items;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] rsl = new Item[size];
+        int rslIndex = 0;
+        for (int index = 0; index < size; index++) {
+            if (key.equals(items[index].getName())) {
+                rsl[rslIndex++] = items[index];
+            }
+        }
+        return Arrays.copyOf(rsl, rslIndex);
+    }
 
     public Item add(Item item) {
         item.setId(ids++);
