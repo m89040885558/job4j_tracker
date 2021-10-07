014f76e8f2d7f7ee39428b552ac955decc4c8ae0
m89040885558 <m89040885558@yandex.ru>
2021-10-07 18:48:47 +0300

    8. Что такое валидация? [#246864 #212536]

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -6,7 +6,6 @@ import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
-
 public class TrackerTest {
     @Test
     public void whenTestFindById() {
