cce66fa64f067f317bd5fe83dc1c776c6ac0f9d9
m89040885558 <m89040885558@yandex.ru>
2021-10-07 18:57:12 +0300

    9. toString [#310018 #212567]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -5,20 +5,11 @@ import java.time.format.DateTimeFormatter;
 
 public class Item {
 
+    private static final DateTimeFormatter FORMATTER
+            = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
     private int id;
     private String name;
     private LocalDateTime created = LocalDateTime.now();
-    private static final DateTimeFormatter FORMATTER
-            = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-
-    @Override
-    public String toString() {
-        return "Item{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", created=" + created +
-                '}';
-    }
 
     public Item() {
     }
@@ -30,6 +21,14 @@ public class Item {
     public Item(int id, String name) {
     this.id = id;
     this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" + "id=" + id +
+                ", name='" + name + '\'' +
+                ", created=" + created.format(FORMATTER) +
+                '}';
     }
 
     public int getId() {
